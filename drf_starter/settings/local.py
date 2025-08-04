79d2f6bb34b6afb95820398742e56f1c77a97148
Emilakesofficial <157388465+Emilakesofficial@users.noreply.github.com>
2025-08-04 15:51:47 +0100

    For deployment

--- drf_starter/settings/local.py
+++ drf_starter/settings/local.py
@@ -1,12 +1,10 @@
 from .base import *
 import os
+import dj_database_url
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.local'))
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default":dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 # Static files (served locally)
--- drf_starter/settings/production.py
+++ drf_starter/settings/production.py
@@ -1,19 +1,14 @@
 from .base import *
 import os
+import dj_database_url
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.local'))
 
 DEBUG = False
 
-DATABASES = {           
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT")
-    } 
+DATABASES = {
+    "default":dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 # Static files (served locally)
 STATIC_URL = '/static/'
@@ -23,32 +18,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-USE_S3 = False
-
-if USE_S3:
-    AWS_ACCESS_KEY_ID = env("S3_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env("S3_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env("S3_BUCKET_NAME")
-    AWS_S3_CUSTOM_DOMAIN = "localhost:9444/ui/mesh"
-    AWS_S3_ENDPOINT_URL = env("S3_ENDPOINT_URL")
-    AWS_S3_USE_SSL = False
-    AWS_S3_URL_PROTOCOL = "http:"
-
-    STORAGES = {
-        "default": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-            "OPTIONS": {
-                "bucket_name": AWS_STORAGE_BUCKET_NAME,
-                "custom_domain": AWS_S3_CUSTOM_DOMAIN,
-                "endpoint_url": AWS_S3_ENDPOINT_URL,
-                "use_ssl": False,
-            },
-        },
-        "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-        },
-    }
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
@@ -57,7 +26,7 @@ EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = env(ALLOWED_HOSTS)
 
 CACHES = {
     'default': {
